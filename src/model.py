305cd8c1b098f179d520f530acea12980d7bad93
tnguyen91 <nmt090102@gmail.com>
2025-06-17 22:42:19 -0700

    add model.py

---
+++ src/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class RBM(nn.Module):
+    def __init__(self, n_visible, n_hidden):
+        super(RBM, self).__init__()
+        self.n_visible = n_visible
+        self.n_hidden = n_hidden
+
+        self.W = nn.Parameter(torch.empty(n_hidden, n_visible))
+        nn.init.xavier_uniform_(self.W)
+        self.v_bias = nn.Parameter(torch.zeros(n_visible))
+        self.h_bias = nn.Parameter(torch.zeros(n_hidden))
+
+    def sample_h(self, v):
+        p_h = torch.sigmoid(torch.mm(v, self.W.t()) + self.h_bias)
+        return p_h, torch.bernoulli(p_h)
+
+    def sample_v(self, h):
+        p_v = torch.sigmoid(torch.mm(h, self.W) + self.v_bias)
+        return p_v, torch.bernoulli(p_v)
