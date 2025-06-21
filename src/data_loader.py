544ce76630e25d9aeff2d662e3faf508faadfb8e
tnguyen91 <nmt090102@gmail.com>
2025-06-20 23:06:07 -0700

    update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,51 +4,30 @@ import pandas as pd
 import numpy as np
 import kagglehub
 import torch
-from torch.utils.data import random_split
+from src.utils import filter_hentai
 
 SEED = 1234
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
 
-def load_anime_data(n_users=5000, n_items=3000):
-    os.environ["KAGGLEHUB_CACHE"] = "./data"
-    path = kagglehub.dataset_download("dbdmobile/myanimelist-dataset")
+os.environ["KAGGLEHUB_CACHE"] = "./data"
 
-    versions_dir = "../data/datasets/dbdmobile/myanimelist-dataset/versions/*/"
-    version_folders = glob.glob(versions_dir)
-    latest_version = sorted(version_folders)[-1].replace("\\", "/")
+def load_anime_dataset():
+    path = kagglehub.dataset_download("hernan4444/anime-recommendation-database-2020")
+    versions_dir = "./data/datasets/hernan4444/anime-recommendation-database-2020/versions/*/"
+    latest_version = sorted(glob.glob(versions_dir))[-1].replace("\\", "/")
 
-    rating_path = os.path.join(latest_version, "users-score-2023.csv")
+    rating_path = os.path.join(latest_version, "rating_complete.csv")
+    anime_path = os.path.join(latest_version, "anime.csv")
+    
+    ratings = pd.read_csv(rating_path)
+    anime = pd.read_csv(anime_path)
 
-    ratings = pd.read_csv(rating_path, chunksize=1500000)
-    ratings = next(ratings)
+    print(ratings.memory_usage(deep=True).sum() / 1e6, "MB")
+    ratings['user_id'] = ratings['user_id'].astype(np.uint32)
+    ratings['anime_id'] = ratings['anime_id'].astype(np.uint32)
+    ratings['rating'] = ratings['rating'].astype(np.int8)
+    print(ratings.memory_usage(deep=True).sum() / 1e6, "MB")
 
-    ratings['liked'] = (ratings['rating'] >= 7).astype(int)
-
-    anime_counts = ratings['anime_id'].value_counts()
-    popular_anime_ids = anime_counts[anime_counts >= 50].index
-    ratings = ratings[ratings['anime_id'].isin(popular_anime_ids)]
-
-    print("Filtered ratings shape:", ratings.shape)
-    print(ratings.head())
-
-    top_users = ratings['user_id'].value_counts().head(n_users).index
-
-    top_anime = ratings['anime_id'].value_counts().head(n_items).index
-
-    filtered_ratings = ratings[
-        ratings['user_id'].isin(top_users) &
-        ratings['anime_id'].isin(top_anime)
-    ]
-
-    user_anime = filtered_ratings.pivot_table(
-        index='user_id',
-        columns='anime_id',
-        values='liked',
-        fill_value=0
-    )
-
-    print(user_anime)
-
-    return user_anime, ratings
+    return filter_hentai(ratings, anime)
