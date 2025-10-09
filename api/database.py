59bb20dffd3a504e0610d94bb22de8a4efb7b086
tnguyen91 <nmt090102@gmail.com>
2025-10-08 19:14:21 -0700

    add database connection module

---
+++ api/database.py
@@ -0,0 +1,31 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True, 
+    pool_size=5,
+    max_overflow=10
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
